eb33cc95b5b0aeb51ce0f14567902452233ce4e2
lougoncharenko <93098869+lougoncharenko@users.noreply.github.com>
2021-12-31 12:28:13 -0500

    Add files via upload

--- calculator app/main.js
+++ calculator app/main.js
@@ -1,82 +1,121 @@
 // algorithm:
 //define calculator with DOM
-let calculator=document.querySelector('table')
-console.log(calculator)
 //define display with DOM
-let display=document.getElementById('result');
-console.log(display);
 //define buttons with DOM (include values)
-let buttons= document.querySelectorAll('input');
-console.log(buttons);
 //let buttonValue=document.getElementsByClassName('button').value;
 
 
-
-
+let currentOperator = null;
+let firstOperand = null;
+let secondOperand = null;
+let result = null;
+const displayArea = document.getElementById("display-area");
+const numberButtons = document.querySelectorAll(".numbers");
+const operators = document.querySelectorAll(".operator");
+const equalSign = document.getElementById("equal");
+const clearButton = document.getElementById("clear");
 
 
 //add function
 function add(num1,num2){
-    //console.log(num+num) ; works
-    return num1+num2;
+    console.log('add')
+    return num1 + num2;
 }
 
 //add(5,5);
 
 //subtract function
 function subtract(num1,num2){
-    //console.log(num1-num2); works
+    console.log("subtract")
     return num1-num2;
 }
 //subtract(7,1);
 
 //multiply function
 function multiply(num1,num2){
-    //console.log(num1*num2) works
+    console.log('multiply')
     return num1*num2;
 }
 //multiply(7,8);
 
 //divide function
 function divide(num1,num2){
-    //console.log(num1/num2) works with integers but not floating numbers
+    console.log('divide')
     return num1/num2;
 }
-//divide(10,2);
-//divide(5/1); displays NaN; fix to show decimals
+
 
 /*operate function(num,num)
 executes add, sub, multiply or divide function 
 runs when the user presses the '=' key*/
 
-function calculate(a,b){
-    buttons.forEach(button =>{
-        button.addEventListener('click', function(){
-            if (button.value==="+"){
-               console.log('add');
-            }if (button.value==="-"){
-                console.log('subtract')
-            } if (button.value==="*"){
-                console.log("multiply");
-            } if (button.value ==="/"){
-                console.log('divide');
-            } else {
-                console.log(button.value)
-        };
-        })
+function operate(num1, num2, operator) {
+    switch(operator) {
+      case "add":
+        return add(num1, num2);
+        break;
+      case "subtract":
+        return subtract(num1, num2);
+        break;
+      case "multiply":
+        return multiply(num1, num2);
+        break;
+      case "divide":
+        return divide(num1, num2);
+        break;
+    }
+  }
+
+ 
+
+     operators.forEach(operator =>{
+        operator.addEventListener('click', function(e){
+            displayArea.innerHTML=operator.value
+            setOperator(operator.value);
+                   
+             })
     });
     
-}
+    numberButtons.forEach(number =>{
+        number.addEventListener('click', function(e){
+            displayArea.innerHTML=number.value
+            setOperand(number.value);
+        
+             })
+    });
 
-calculate();
+    function setOperator(operator){
+        if (currentOperator=null){
+          currentOperator=operator;
+        }else {
+            return operate(Number(firstOperand),Number(secondOperand), currentOperator)
+        }
+
+    }
+    function setOperand(value) {
+        if (firstOperand == null) {
+          firstOperand = value;
+        } else {
+          secondOperand = value;
+        }
+      }
+    
+  
+   
 
 
-//define a clear button
-//define a clear function
 
 //create an = button
 //create an event listener for = to run operate function
-function clear(){
-    document.getElementById('clear').value = " ";
-}
+equalSign.addEventListener('click', function(){
+   return 
+  
+});
+
+
+//define a clear button  event listener with a clear function
+clearButton.addEventListener('click', function(){
+    displayArea.innerHTML=" "
+});
+
 
